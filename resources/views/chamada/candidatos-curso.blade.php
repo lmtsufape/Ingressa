c15fee243629cd8c82c94a027ef209c386f66114
Carlos <carlos.andre1270@gmail.com>
2021-12-10 12:40:57 -0300

    View que lista os candidatos

--- resources/views/chamada/candidatos-curso.blade.php
+++ resources/views/chamada/candidatos-curso.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="fundo2 px-5">
-        <div class="row justify-content-center" >
+        {{-- <div class="row justify-content-center" >
             <div class="col-md-9 p-0" style="text-align: right">
                 <a class="btn botao my-2 py-1" href="{{route('chamadas.candidatos', ['sisu_id' => $chamada->sisu->id, 'chamada_id' => $chamada->id])}}"> <span class="px-4">Voltar</span></a>
             </div>
@@ -110,8 +110,171 @@
                 </div>
             </div>
             
+        </div> --}}
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-md-9">
+                    <div class="col-md-12 shadow-sm">
+                        <div class="row justify-content-center">
+                            <div class="col-md-12 cabecalhoCurso p-2 px-3 align-items-center" style="background-color: {{$curso->cor_padrao != null ? $curso->cor_padrao : 'black'}}"> 
+                              <div class="row justify-content-between">
+                                <div class="d-flex align-items-center justify-content-between">
+                                    <div class="d-flex align-items-center">
+                                      <img src="{{asset('storage/'.$curso->icone)}}" 
+                                      alt="" width="40" class="img-flex">
+                                      <div>
+                                        <span class="tituloTabelas ps-1 mb-0 pb-0">{{$curso->nome}} - {{$turno}}</span>
+                                        <div class="ps-1 mt-0 pt-0" style="font-size: 14px; color: white;">
+                                            <span style="font-weight: bolder;">Ordenação:
+                                                @switch($ordem)
+                                                    @case('name')
+                                                        Nome do candidato
+                                                        @break
+                                                    @case('cota')
+                                                        Cota
+                                                        @break
+                                                    @case('status')
+                                                        Status da inscrição
+                                                        @break
+                                                    @default
+                                                        Nome do candidato
+                                                        @break
+                                                @endswitch
+                                            </span> 
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <div>
+                                      <a class="" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <img width="35" src="{{asset('img/Subtração 2.svg')}}">
+                                      </a>
+                                      <ul class="dropdown-menu px-2" aria-labelledby="dropdownMenu2">
+                                        <div class="form-check link-ordenacao">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" @if($ordem == null || $ordem == 'name') checked @endif>
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                Nome do candidato
+                                            </label>
+                                            <a id="link-ordem-name" href="{{route('chamadas.candidatos.curso', ['sisu_id' => $sisu->id, 'chamada_id' => $chamada->id, 'curso_id' => $curso->id])}}?ordem=name"></a>
+                                        </div>
+                                        <div class="form-check link-ordenacao">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"  @if($ordem == 'cota') checked @endif>
+                                            <label class="form-check-label" for="flexRadioDefault2">
+                                                Cota
+                                            </label>
+                                            <a id="link-ordem-cota" href="{{route('chamadas.candidatos.curso', ['sisu_id' => $sisu->id, 'chamada_id' => $chamada->id, 'curso_id' => $curso->id])}}?ordem=cota"></a>
+                                        </div>
+                                        <div class="form-check link-ordenacao">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"  @if($ordem == 'status') checked @endif>
+                                            <label class="form-check-label" for="flexRadioDefault2">
+                                                Status da inscrição
+                                            </label>
+                                            <a id="link-ordem-status" href="{{route('chamadas.candidatos.curso', ['sisu_id' => $sisu->id, 'chamada_id' => $chamada->id, 'curso_id' => $curso->id])}}?ordem=status"></a>
+                                          </div>
+                                      </ul>
+                                    </div>
+                                </div>
+                                
+                              </div>
+                            </div>
+                        </div>
+    
+                        <div class="row justify-content-center">
+                            <div class="col-md-12 corpo p-2 px-3"> 
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Nome</th>
+                                            <th class="text-center">Cota</th>
+                                            <th class="text-center">Status</th>
+                                            <th class="text-center">Ações</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($candidatos as $i => $candidato)
+                                            <tr>
+                                                <th class="align-middle"> {{$i+1}}</th>
+                                                <td class="align-middle">{{$candidato->candidato->user->name}}</td>
+                                                <td class="align-middle text-center">{{$candidato->cota->cod_cota}}</td>
+                                                <td class="align-middle text-center">
+                                                    <div class="btn-group">
+                                                        @if($candidato->candidato->user->email != null)
+                                                            <img src="{{asset('img/icon_aprovado_verde.svg')}}" alt="..." width="25px" data-toggle="tooltip" data-placement="top" title="Cadastro do candidato concluído">
+                                                        @else
+                                                            <img src="{{asset('img/icon_reprovado_vermelho.svg')}}" alt="..." width="25px" data-toggle="tooltip" data-placement="top" title="Cadastro do candidato não concluído">
+                                                        @endif
+
+                                                        @if($candidato->status == \App\Models\Inscricao::STATUS_ENUM['documentos_aceitos'])
+                                                            <img src="{{asset('img/icons-document-blue.png')}}" alt="..." width="25px" data-toggle="tooltip" data-placement="top" title="Documentos aceitos">
+                                                        @elseif($candidato->status == \App\Models\Inscricao::STATUS_ENUM['documentos_enviados'])
+                                                            <img src="{{asset('img/icons-document-yellow.png')}}" alt="..." width="25px" data-toggle="tooltip" data-placement="top" title="Documentos enviados">
+                                                        @elseif($candidato->status == \App\Models\Inscricao::STATUS_ENUM['documentos_requeridos'])
+                                                            <img src="{{asset('img/icons-document-red.png')}}" alt="..." width="25px" data-toggle="tooltip" data-placement="top" title="Documentos requeridos">
+                                                        @endif
+                                                    </div>
+                                                </td>
+                                                <td class="align-middle text-center"><a class="btn btn-sm" href="{{route('inscricao.show.analisar.documentos', ['sisu_id' => $chamada->sisu->id, 'chamada_id' => $chamada->id, 'curso_id' => $curso->id, 'inscricao_id' => $candidato->id])}}" style="background-color: #1CE8B1; color: white; font-size: 14px; font-weight: bolder;">Avaliar</a></td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                <a href="{{route('chamadas.candidatos', ['sisu_id' => $chamada->sisu->id, 'chamada_id' => $chamada->id])}}" class="btn botao my-2 py-1" type="submit"> <span class="px-4">Voltar</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="col-md-12 shadow-sm p-2 px-3" style="background-color: white; border-radius: 00.5rem;">
+                        <div style="font-size: 21px;" class="tituloModal">
+                            Legenda
+                        </div>
+                        <ul class="list-group list-unstyled">
+                            <li>
+                                <div title="Cadastro do candidato concluído" class="d-flex align-items-center listagemLista my-2 pt-1 pb-3">
+                                    <img class="aling-middle" width="33" src="{{asset('img/icon_aprovado_verde.svg')}}" alt="icone-busca">
+                                    <div style="font-size: 13px;" class="tituloLista aling-middle mx-3">
+                                        Cadastro do candidato concluído
+                                    </div>                    
+                                </div>
+                            </li>
+                            <li>
+                                <div title="Cadastro do candidato não concluído" class="d-flex align-items-center listagemLista my-2 pt-1 pb-3">
+                                    <img class="aling-middle" width="33" src="{{asset('img/icon_reprovado_vermelho.svg')}}" alt="icone-busca">
+                                    <div style="font-size: 13px;" class="tituloLista aling-middle mx-3">
+                                        Cadastro do candidato não concluído
+                                    </div>                    
+                                </div>
+                            </li>
+                            <li>
+                                <div title="Documentos aceitos" class="d-flex align-items-center listagemLista my-2 pt-1 pb-3">
+                                    <img class="aling-middle" width="33" src="{{asset('img/icons-document-blue.png')}}" alt="icone-busca">
+                                    <div style="font-size: 13px;" class="tituloLista aling-middle mx-3">
+                                        Documentos aceitos
+                                    </div>                    
+                                </div>
+                            </li>
+                            <li>
+                                <div title="Documentos enviados" class="d-flex align-items-center listagemLista my-2 pt-1 pb-3">
+                                    <img class="aling-middle" width="33" src="{{asset('img/icons-document-yellow.png')}}" alt="icone-busca">
+                                    <div style="font-size: 13px;" class="tituloLista aling-middle mx-3">
+                                        Documentos enviados
+                                    </div>                    
+                                </div>
+                            </li>
+                            <li>
+                                <div title="Documentos requeridos" class="d-flex align-items-center listagemLista my-2 pt-1 pb-3">
+                                    <img class="aling-middle" width="33" src="{{asset('img/icons-document-red.png')}}" alt="icone-busca">
+                                    <div style="font-size: 13px;" class="tituloLista aling-middle mx-3">
+                                        Documentos requeridos
+                                    </div>                    
+                                </div>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-        
     </div>
 </x-app-layout>
 
@@ -124,5 +287,10 @@
         var modal = $(this)
         modal.find('.modal-title').text('Deseja ' + texto + nome)
         modal.find('.modal-body input').val(recipient)
-    })
+    });
+
+    $('.link-ordenacao').click(function() {
+        link = this.children[2].href;
+        window.location = link;
+    });
 </script>
