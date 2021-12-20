aa7133f6a12a11179b23bb991617ef724265442e
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2021-12-20 09:21:16 -0300

    select multiple

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,18 +17,18 @@
         <link href="{{asset('bootstrap/css/bootstrap.css')}}" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('css/main.css')}}">
 
-
+        <link rel="stylesheet" href="{{asset('bootstrap-select/css/bootstrap-select.min.css')}}">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
         <script src="https://unpkg.com/@popperjs/core@2" ></script>
         <script src="{{asset('bootstrap/js/bootstrap.js')}}"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.4.2/umd/popper.min.js"></script>
 
         <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
+        <script src="{{asset('bootstrap-select/js/bootstrap-select.min.js')}}"></script>
 
         <script src="{{asset('ckeditor/ckeditor.js')}}"></script>
     </head>
