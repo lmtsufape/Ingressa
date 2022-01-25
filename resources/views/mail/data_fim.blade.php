e1075dfbcec2da6bb475d229e5b247f3063bcd8e
Carlos André de Almeida Cavalcante <carlos.andre1270@gmail.com>
2022-01-25 12:36:09 -0300

    envio de email automatico para os candidatos quando a data de envio esta encerrando

--- resources/views/mail/data_fim.blade.php
+++ resources/views/mail/data_fim.blade.php
@@ -4,12 +4,12 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Período aberto</title>
+        <title>Período encerramento</title>
     </head>
     <body style="color: black;">
         <p>Olá {{$user->name}},</p>
         <p>
-            Está aberto o período para {{$data->getNomeEvento()}}, confira mais informações clicando <a href="{{route('index')}}">aqui</a>.
+            Encerra hoje o período para {{$data->getNomeEvento()}}, confira mais informações clicando <a href="{{route('index')}}">aqui</a>.
         </p>
     </body>
 </html>
