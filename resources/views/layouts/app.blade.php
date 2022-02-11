0b341a5cabe1fd6b74e4d701d54dfe862dd23e39
KelwinJonas <kelwinjonas@gmail.com>
2022-02-11 15:00:46 -0300

    Mensagem de cadastro e desabilitar submit button para tarefa sincrona

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,5 +62,20 @@
         @livewireScripts
         <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         @stack('scripts')
+
+        <script>
+            $(document).ready(function () {
+                var btn = document.getElementsByClassName("submeterFormBotao");
+                if(btn.length > 0){
+                    $(document).on('submit', 'form', function() {
+                        $('button').attr('disabled', 'disabled');
+                        for (var i = 0; i < btn.length; i++) {
+                        btn[i].textContent = 'Aguarde...';
+                        btn[i].style.backgroundColor = btn[i].style.backgroundColor + 'd8';
+                    }
+                    });
+                }
+            })
+        </script>
     </body>
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -29,5 +29,20 @@
             {{ $slot }}
         </div>
         @component('layouts.footer')@endcomponent
+
+        <script>
+            $(document).ready(function () {
+                var btn = document.getElementsByClassName("submeterFormBotao");
+                if(btn.length > 0){
+                    $(document).on('submit', 'form', function() {
+                        $('button').attr('disabled', 'disabled');
+                        for (var i = 0; i < btn.length; i++) {
+                        btn[i].textContent = 'Aguarde...';
+                        btn[i].style.backgroundColor = btn[i].style.backgroundColor + 'd8';
+                    }
+                    });
+                }
+            })
+        </script>
     </body>
 </html>
