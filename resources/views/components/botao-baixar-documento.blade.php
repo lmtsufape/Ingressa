993a9134846f7b8a576552cb31390358acaa3462
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-02-20 08:49:37 -0300

    componentes dos botoes de enviar e baixar doc

---
+++ resources/views/components/botao-baixar-documento.blade.php
@@ -0,0 +1,9 @@
+<a wire:click="baixar('{{$documento}}')"
+    title="Baixar documento"
+    target="_blank"
+    style="cursor:pointer;">
+    <img src="{{asset('img/download2.svg')}}"
+        alt="arquivo atual"
+        width="30"
+        class="img-flex">
+</a>
