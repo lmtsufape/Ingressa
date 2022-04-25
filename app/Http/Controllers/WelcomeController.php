d7a024d35d9fcea7c1f55fe0a4f275fa71c8a388
KelwinJonas <kelwinjonas@gmail.com>
2022-04-25 13:28:12 -0300

    aumentando periodo de exibicao de edicao na tela inicial

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -14,7 +14,7 @@ class WelcomeController extends Controller
 {
     public function index()
     {
-        $intervalo_inicio = now()->subMonth(2);
+        $intervalo_inicio = now()->subMonth(3);
         $intervalo_fim = now()->addMonth(2);
         
         $edicao_atual = Sisu::where([['created_at', '>=', $intervalo_inicio], ['created_at', '<=', $intervalo_fim]])->first();
