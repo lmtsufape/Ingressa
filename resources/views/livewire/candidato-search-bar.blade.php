d6f5393b3a064c23335eb4421261627d24176f8e
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2023-02-28 14:56:20 -0300

    melhora css

--- resources/views/livewire/candidato-search-bar.blade.php
+++ resources/views/livewire/candidato-search-bar.blade.php
@@ -1,21 +1,19 @@
-<div class="position-relative" x-data>
+<div x-data>
     <input 
-        type="text" 
-        class="form-control campoDeTexto" 
-        placeholder="Procurar Candidato..." 
-        wire:model="texto" 
-        wire:keydown.escape="limpar" 
-        wire:keydown.tab="limpar" 
-        wire:keydown.arrow-up="decrementHighlight" 
-        wire:keydown.arrow-down="incrementHighlight" 
+        type="text"
+        class="form-control campoDeTexto"
+        placeholder="Procurar Candidato..."
+        wire:model="texto"
+        wire:keydown.escape="limpar"
+        wire:keydown.tab="limpar"
         wire:keydown.enter="selecionarCandidato"
         @click.outside="$wire.limpar()"
     />
     @if(!empty($texto))
-    <div class="position-fixed list-group">
+    <div class="dropdown-menu d-block">
         @if(!empty($inscricoes))
         @foreach($inscricoes as $i => $inscricao)
-        <a href="{{ $links->get($i) }}" class="list-group-item">{{ $inscricao->candidato->no_inscrito }} - {{$inscricao->sisu->edicao}}</a>
+        <a href="{{ $links->get($i) }}" class="dropdown-item">{{ $inscricao->candidato->no_inscrito }} - {{$inscricao->sisu->edicao}}</a>
         @endforeach
         @else
         <div class="">Nenhum candidato!</div>
