70142172df0eeb75974d93f7bf21d3da09434f4e
Rener1 <rener.tome123@gmail.com>
2024-01-09 20:40:27 -0300

    Correção no corpo do email.

--- resources/views/mail/declaracaoPreMatricula.blade.php
+++ resources/views/mail/declaracaoPreMatricula.blade.php
@@ -1,3 +1,1 @@
-DECLARAÇÃO
-
-Declaramos, para os devidos fins, que {{$incricao->candidato->no_inscrito}}, CPF {{$incricao->candidato->nu_cpf_inscrito}}, classificado(a) no Sistema de Seleção Unificado 2024 no semestre {{$inscricao->semestre_entrada}}, curso {{$inscricao->no_curso}}, turno {{$inscricao->ds_turno}}, efetivou sua pré-matrícula com início das aulas previsto para <>.
+Declaramos, para os devidos fins, que {{$incricao->candidato->no_inscrito}}, CPF {{$incricao->candidato->nu_cpf_inscrito}}, classificado(a) no Sistema de Seleção Unificado 2024 no semestre {{$inscricao->semestre_entrada}}, curso {{$inscricao->no_curso}}, turno {{$inscricao->ds_turno}}.
