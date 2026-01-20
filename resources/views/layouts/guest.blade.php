e742ad0aac929221ed87aae221bb2a8ae2dd6f12
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2026-01-20 15:58:04 -0300

    feat: adiciona um botão para visualizar ou ocultar a senha

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,6 +22,8 @@
         <script src="{{asset('bootstrap/js/bootstrap.js')}}" defer></script>
         <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
         <script src="{{asset('js/jquery.mask.min.js')}}"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
     </head>
     <body class="font-sans antialiased">
         <!-- Barra Brasil -->
@@ -57,5 +59,7 @@
         </script>
         <script defer="defer" src="//barra.brasil.gov.br/barra.js" type="text/javascript"></script>
         @livewireScripts
+        @stack('scripts')
+
     </body>
 </html>
