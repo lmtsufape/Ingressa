fd4cee3a6a2ed5f2d69b4471a3c455a88e9da0cd
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2026-04-06 16:32:14 -0300

    fix: corrige o nome da coluna no dropColumn

--- database/migrations/2026_04_06_110148_add_cod_siga_to_cursos_table.php
+++ database/migrations/2026_04_06_110148_add_cod_siga_to_cursos_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('cursos', function (Blueprint $table) {
-            $table->dropColumn('cursos');
+            $table->dropColumn('cod_siga');
         });
     }
 };
